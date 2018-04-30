742ea201103720b8ac11d9df34fb5b2abeecf17e
Harm Aarts <harmaarts@gmail.com>
2018-04-30 11:35:59 +0200

    Add more calls to cli example

--- examples/cli.py
+++ examples/cli.py
@@ -1,8 +1,6 @@
 import asyncio
 import sys
 import pylibbitcoin.client
-import binascii
-import struct
 
 
 def block_header(client):
@@ -14,9 +12,34 @@ def last_height(client):
     return client.last_height()
 
 
+def block_height(client):
+    hash = sys.argv[2]
+    return client.block_height(hash)
+
+
+def transaction(client):
+    hash = sys.argv[2]
+    return client.transaction(hash)
+
+
+def transaction_index(client):
+    hash = sys.argv[2]
+    return client.transaction_index(hash)
+
+
+def spend(client):
+    hash = sys.argv[2]
+    index = int(sys.argv[3])
+    return client.spend(hash, index)
+
+
 commands = {
     "last_height": last_height,
-    "block_header": block_header
+    "block_header": block_header,
+    "block_height": block_height,
+    "transaction": transaction,
+    "transaction_index": transaction_index,
+    "spend": spend,
 }
 
 
@@ -30,13 +53,15 @@ async def main():
     client = pylibbitcoin.client.Client("tcp://mainnet.libbitcoin.net:9091")
 
     ec, result = await commands[sys.argv[1]](client)
+    # print(bitcoin.core.CBlockHeader.deserialize(result))
     # unpack according to
     # https://en.bitcoin.it/wiki/Protocol_documentation#Block_Headers
-    version, prev, root, timestamp, bits, nounce = \
-        struct.unpack('<I32s32sIII', result)
-    print(binascii.hexlify(prev[::-1]).decode('utf8'))
-    print(binascii.hexlify(root[::-1]).decode('utf8'))
-
+    # version, prev, root, timestamp, bits, nounce = \
+    #     struct.unpack('<I32s32sIII', result)
+    # print(binascii.hexlify(prev[::-1]).decode('utf8'))
+    # print(binascii.hexlify(root[::-1]).decode('utf8'))
+    print("Error code: %s" % ec)
+    print(result)
     client.stop()
 
 if __name__ == '__main__':
